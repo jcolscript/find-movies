27e90451e0a5b53e5952e8e8f79e48b0a1f8fabf
José Carlos <jcolscript@gmail.com>
2020-10-22 05:23:27 -0300

    refactoring movie card component

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
-import { IMoviesSimpleData } from '../../infra/interfaces/movies.interfaces'
+import { IMoviesSimpleData } from '../../infra/interfaces/movies.interfaces';
+
+import notposter from '../../assets/not-poster.svg';
 
 import { AnimationContainer } from './styles';
 
 import Tooltip from '../Tooltip/index'
 
-const MovieCard: React.FC<IMoviesSimpleData> = ({Poster, Title, Type, Year, imdbID}) => {
+const MovieCard: React.ForwardRefExoticComponent<IMoviesSimpleData> = React.forwardRef((
+  {Poster, Title, Type, Year, imdbID},
+   ref: React.Ref<HTMLDivElement >
+  ) => {
   return (
-      <AnimationContainer className="col-md-4">
-        <div className="card mb-4 shadow-sm">
+      <AnimationContainer className="col-md-4" ref={ref}>
+        <div className="card mb-4 shadow-sm" >
           <Tooltip Title={Title} Type={Type} Year={Year} imdbID={imdbID} Poster={Poster}>
-            <img className="img-fluid" src={Poster}  alt={Title}/>
+            <img className="img-fluid" src={Poster === "N/A" ? notposter : Poster}  alt={Title}/>
           </Tooltip>
         </div>
       </AnimationContainer>
   );
-}
+})
 
 export default MovieCard;
