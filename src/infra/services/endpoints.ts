66d82f6f2d86f4df90056ca638e329e47d979239
José Carlos <jcolscript@gmail.com>
2020-10-22 05:22:13 -0300

    refactoring service

--- src/infra/services/endpoints.ts
+++ src/infra/services/endpoints.ts
@@ -1,8 +1,8 @@
 import env from '../utils/enviroments.config';
 
 const endpoints = {
-  getMovies: (name: string) => `${env.REACT_OMDB_API}&s=${name}`,
-  getMovie: (imdb_id: number) => `${env.REACT_OMDB_API}&i=${imdb_id}`,
+  getMovies: (name: string, page: number) => `${env.REACT_OMDB_API}&s=${name}&page=${page}`,
+  getMovie: (imdb_id: string) => `${env.REACT_OMDB_API}&i=${imdb_id}`,
 };
 
 export default endpoints;
