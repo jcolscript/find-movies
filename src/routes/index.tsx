3a8dbeebfe0b9d3fae0e268f9fbbade90f818776
José Carlos <jcolscript@gmail.com>
2020-10-22 05:21:54 -0300

    adding movie route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Main from '../pages/Main';
+import Movie from '../pages/Movie';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Main}/>
+    <Route path="/movie/:username+" exact component={Movie}/>
   </Switch>
 );
 
